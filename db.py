2a505fd07bc2409443cec7981807a9b4c61d3503
JuanCote <lyy1707@gmail.com>
2022-08-30 16:25:16 +0300

    add counter checker

---
+++ db.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+MONGODB_URI = 'mongodb+srv://JuanCote:tfkn7C64u55PFtl4@cluster0.lecracw.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient(MONGODB_URI)
+db = client.Sanyok
+
+cards = db.cards
---
+++ main.py
@@ -0,0 +1,137 @@
+import pytz
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+from bson.objectid import ObjectId
+from starlette.responses import JSONResponse
+
+from db import cards
+
+
+app = FastAPI()
+
+timezone = pytz.timezone('Europe/Moscow')
+
+
+class Card(BaseModel):
+    title: str
+    date: datetime
+    counter: int
+
+
+class UpdateCard(BaseModel):
+    title: Optional[str]
+    counter: Optional[int]
+
+
+class ResponseCard(BaseModel):
+    id: str
+    title: str
+    date: datetime
+    counter: int
+
+
+@app.get('/api/get_cards', responses={
+    200: {
+        'description': 'Gives back all cards',
+        'content': {
+            'application/json': {
+                'example': {'cards': 'list of cards'}
+            }
+        }
+    }
+})
+async def get_cards():
+    cursor = cards.find({'is_deleted': False})
+
+    result = {'cards': []}
+
+    for card in cursor:
+        card['id'] = str(card['_id'])  # converting id from ObjectId to string
+
+        current_time = datetime.now(timezone).replace(tzinfo=None)
+
+        difference = int((current_time - card['viewed']).total_seconds() // 3600)
+        if difference > 24:
+            card['counter'] = 0
+            cards.update_one({'_id': card['_id']}, {'$set': {'counter': 0}})
+        elif current_time.day != card['viewed'].day:
+            card['counter'] = 0
+            cards.update_one({'_id': card['_id']}, {'$set': {'counter': 0}})
+
+        cards.update_one({'_id': card['_id']}, {'$set': {'viewed': current_time}})
+
+        del card['is_deleted'], card['_id'], card['viewed']
+        result['cards'].append(card)
+
+    return result
+
+
+@app.post('/api/add_card', response_model=ResponseCard)
+async def add_card(card: Card):
+    card = card.dict()
+    card['is_deleted'] = False
+    card['viewed'] = datetime.now(timezone)
+    cards.insert_one(card)
+
+    card['id'] = str(card['_id'])  # converting id from ObjectId to string
+    del card['is_deleted'], card['_id'], card['viewed']
+
+    return card
+
+
+@app.delete(
+    '/api/delete_card/{card_id}',
+    responses={
+        404: {
+            "description": "The card was not found",
+            "content": {
+                "application/json": {
+                    "example": {"message": "non-existent card"}
+                }
+            }
+        },
+        200: {
+            "description": "Successful removal",
+            "content": {
+                "application/json": {
+                    "example": {"message": "deleted"}
+                }
+            },
+        },
+    },
+)
+async def delete_card(card_id: str):
+    try:
+        cards.update_one({'_id': ObjectId(card_id)}, {'$set': {'is_deleted': True}})
+    except:
+        return JSONResponse(status_code=404, content={'message': 'non-existent card'})
+
+    return {"message": "deleted"}
+
+
+@app.put('/api/update_card/{card_id}', response_model=ResponseCard, responses={
+    404: {
+        "description": "The card was not found",
+        "content": {
+            "application/json": {
+                "example": {'message': 'non-existent card'}
+            }
+        }
+    }
+})
+async def update_card(card_id: str, card: UpdateCard):
+    card = card.dict(exclude_unset=True)  # getting a dictionary with only the entered fields
+
+    try:
+        cards.update_one({'_id': ObjectId(card_id)}, {'$set': card})
+    except:
+        return JSONResponse(status_code=404, content={'message': 'non-existent card'})
+
+    new_card = cards.find_one(ObjectId(card_id))
+
+    new_card['id'] = str(new_card['_id'])
+    del new_card['_id'], new_card['is_deleted'], new_card['viewed']
+
+    return new_card

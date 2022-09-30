8151290ea64010c62d8c58822269ea4c0e12112f
JuanCote <lyy1707@gmail.com>
2022-09-30 17:29:13 +0300

    ws test

--- main.py
+++ main.py
@@ -336,15 +336,9 @@ async def ws_test():
     return HTMLResponse(html)
 
 
-@app.websocket("api/ws/")
+@app.websocket("/api/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    client_id = 123
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")

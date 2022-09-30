2a60c6e6a6609aad11f432156913328cff091913
JuanCote <lyy1707@gmail.com>
2022-09-30 17:04:43 +0300

    ws test

--- test_file.py
+++ test_file.py
@@ -13,7 +13,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var ws = new WebSocket("ws://mobile-app-for-sanyok.herokuapp.com/api/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')

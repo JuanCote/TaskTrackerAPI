c18752d23d2fbf1b4ce8ce5d3c33cb3001bbb7a9
JuanCote <lyy1707@gmail.com>
2022-09-30 18:14:16 +0300

    ws test

--- main.py
+++ main.py
@@ -56,13 +56,13 @@ class ConnectionManager:
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
+        print(self.active_connections)
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
@@ -343,7 +343,7 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}")
+            # await manager.send_personal_message(f"You wrote: {data}", websocket)
             await manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)

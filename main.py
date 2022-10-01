4d2e9cb4edb47b731e85eb86165733079a77e5f0
JuanCote <lyy1707@gmail.com>
2022-10-01 16:57:41 +0300

    ws test

--- main.py
+++ main.py
@@ -29,20 +29,21 @@ class ConnectionManager:
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
-        print(self.active_connections)
+        active_connections.append(websocket)
+        print(active_connections)
 
     def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+        active_connections.remove(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, message: str):
-        for connection in self.active_connections:
+        for connection in active_connections:
             await connection.send_text(message)
 
 
+active_connections: List[WebSocket] = []
 manager = ConnectionManager()
 
 

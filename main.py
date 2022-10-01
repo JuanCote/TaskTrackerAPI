dab867e3d54d795d0898ecfcd750f1a3e5c3b378
JuanCote <lyy1707@gmail.com>
2022-10-01 17:19:19 +0300

    ws test

--- main.py
+++ main.py
@@ -27,21 +27,21 @@ ACCESS_TOKEN_EXPIRE_DAYS = 30
 class ConnectionManager:
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        active_connections.append(websocket)
+        active_connections.update({websocket: '2'})
         print(active_connections)
 
     def disconnect(self, websocket: WebSocket):
-        active_connections.remove(websocket)
+        active_connections.pop(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, message: str):
-        for connection in active_connections:
-            await connection.send_text(message)
+        for key, connection in active_connections.items():
+            await key.send_text(message)
 
 
-active_connections = []
+active_connections = {}
 manager = ConnectionManager()
 
 

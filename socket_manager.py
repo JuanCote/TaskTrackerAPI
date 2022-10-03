327f0093d6fc6ef3b22d1657107573807aa45f78
JuanCote <lyy1707@gmail.com>
2022-10-03 10:47:54 +0300

    ws test

--- socket_manager.py
+++ socket_manager.py
@@ -1,53 +1,34 @@
 import asyncio
 import json
 from datetime import datetime
+from typing import List
+
+import starlette.websockets
 
 from db import websockets
 
 from fastapi import WebSocket
 
 
-async def add_socket_to_list(websocket: WebSocket):
-    with open('sockets.json', 'r') as file:
-        data = json.load(file)
-
-    data.update({int(datetime.now().timestamp()): str(websocket)})
-
-    with open('sockets.json', 'w') as file:
-        json.dump(data, file)
-
-    return data
-
-
-def delete_socket_to_list(websocket: WebSocket):
-    with open('sockets.json', 'r') as file:
-        data = json.load(file)
-
-    with open('sockets.json', 'w') as file:
-        json.dump(data, file)
-
-    return data
-
-
 class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+        print(id(self.active_connections))
+
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-
-        task = asyncio.create_task(add_socket_to_list(websocket))
-        await task
-
-        if task:
-            print(task)
-            task.cancel()
+        self.active_connections.append(websocket)
+        print(id(self.active_connections))
 
     def disconnect(self, websocket: WebSocket):
-        print(delete_socket_to_list(websocket))
+        self.active_connections.remove(websocket)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, message: str):
-        pass
+        for connection in self.active_connections:
+            await connection.send_text(message)
 
 
 manager = ConnectionManager()

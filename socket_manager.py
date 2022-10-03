6e38eb99f7626ace67c7b670177def87aadbfc1f
JuanCote <lyy1707@gmail.com>
2022-10-03 11:48:18 +0300

    ws test

--- socket_manager.py
+++ socket_manager.py
@@ -17,6 +17,7 @@ class ConnectionManager:
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         active_connections.append(websocket)
+        print(globals())
         print(id(active_connections))
 
     def disconnect(self, websocket: WebSocket):
